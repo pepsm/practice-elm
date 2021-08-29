1e8878dcef91351cd20b9b3939556df055f03373
pepsm <peps.mihailov@gmail.com>
2021-08-29 19:34:36 +0200

    Collections

---
+++ part1/src/Main.elm
@@ -0,0 +1,17 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+main =
+    div [ class "content" ]
+        [ header []
+            [ h1 [] [ text "ElmHub" ]
+            , span
+                [ class "tagline" ]
+                [ text
+                    "Like GitHub, but for Elm things."
+                ]
+            ]
+        ]
